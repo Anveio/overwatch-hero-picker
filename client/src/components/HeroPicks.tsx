b6e15809e672fa1640d54bf15d4debf187825461
Shovon Hasan <im.shovonhasan@gmail.com>
2017-07-19 00:34:21 -0400

    HeroPicks are now a component generated through React

--- client/src/components/HeroPicks.tsx
+++ client/src/components/HeroPicks.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
 
+const players = [
+  'Lukosa',
+  'Jerm10297',
+  'Manlton',
+  'Dann',
+  'Madh20rat',
+  'Gecko'
+];
+
 export default () => {
+  const playerPicks = () => {
+    return players.map(player => {
+      return (
+        <div className="selection" key={player}>
+          <div className="hero" />
+          <div className="info">
+            <div className="name">Dann</div>
+            <div className="hero-dropdown" />
+          </div>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="selections">
-      <div className="selection">
-        <div className="hero" />
-        <div className="info">
-          <div className="name">Dann</div>
-          <div className="hero-dropdown" />
-        </div>
-      </div>
+      {playerPicks()}
     </div>
   );
 };
